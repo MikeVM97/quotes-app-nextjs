d331e81dd0737b4d8bf31be2c81ede8b1b671112
Mike <vivancomayta@gmail.com>
2023-10-20 12:13:28 -0500

    send json response fixed

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 import getAllQuotes from "@/libs/getAllQuotes";
 
 export async function GET(request: Request) {
   const quotes = await getAllQuotes();
 
-  return NextResponse.json(quotes);
+  return Response.json(quotes);
 }
