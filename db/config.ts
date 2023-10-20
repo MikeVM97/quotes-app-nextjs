cefe4d8dde58ea0197e8ab3b79848e963b003256
Mike <vivancomayta@gmail.com>
2023-10-20 13:03:39 -0500

    env vars using prefixes NEXT_PUBLIC_

--- db/config.ts
+++ db/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  host: process.env.NEXT_PUBLIC_DATABASE_HOST,
+  username: process.env.NEXT_PUBLIC_DATABASE_USERNAME,
+  password: process.env.NEXT_PUBLIC_DATABASE_PASSWORD,
 }
